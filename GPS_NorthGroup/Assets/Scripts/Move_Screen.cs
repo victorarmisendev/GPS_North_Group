b79a03eefc0e1a229ec1636c8b70a8d796c15d29
Victor Armisen Capo <victor.armisen.developer@gmail.com>
2018-10-23 15:16:53 +0200

    Nuevos cambios
    Botones y pantallas

--- GPS_NorthGroup/Assets/Scripts/Move_Screen.cs
+++ GPS_NorthGroup/Assets/Scripts/Move_Screen.cs
@@ -5,16 +5,14 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Move_Screen : MonoBehaviour {
-
 	void Update ()
     {
+        //If scan can be passed by just touching the screen.
         Touch touch = Input.GetTouch(0);
-
         if(touch.position.x > 0)
         {
             //Scene manager loader: Load the scene by an index or name.
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);       
-        }
-     
+        }    
     }
 }
