992cab321975fe5ada6250bdc14646e50e2b66b9
Victor Armisen Capo <victor.armisen.developer@gmail.com>
2018-10-23 03:39:18 +0200

    Primer prueba con Unity: App Input and Canvas
    Determinar el tamaño automatico de la pantalla de la aplicación y su correcto uso de los inputs para poder interaccionar con ella.

---
+++ GPS_NorthGroup/Assets/Scripts/Move_Screen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Move_Screen : MonoBehaviour {
+
+	void Update ()
+    {
+        Touch touch = Input.GetTouch(0);
+
+        if(touch.position.x > 0)
+        {
+            //Scene manager loader: Load the scene by an index or name.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);       
+        }
+     
+    }
+}
---
+++ GPS_NorthGroup/Assets/Scripts/Touch_Screen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Touch_Screen : MonoBehaviour {
+
+    public Text altitude;
+
+    void Update () {
+
+        Touch touch = Input.GetTouch(0);
+        altitude.text = "Altitude is equal to: " + touch.position;
+
+    }
+}
